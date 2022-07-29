2d9291ae01f3bfd8b862f1dc1e0f7fe6d5b4c918
Panchesco <richard@panchesco.com>
2022-07-28 23:33:53 -0700

    Adds tailwindcss ui

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,7 +18,7 @@
 
             <div class="w-full p-6">
                 <p class="text-gray-700">
-                    You are logged in!
+                    
                 </p>
             </div>
         </section>
